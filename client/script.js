d7f76abe2ef44da6e6cd8f146d26869e908d9d22
Entita <entitak@gmai.com>
2021-07-28 18:37:03 +0200

    test

--- client/script.js
+++ client/script.js
@@ -31,10 +31,35 @@ var app = new Vue({
         playerImage.src = 'img/sprite_sheet.png'
         const spriteWidth = 575
         const spriteHeight = 523
+        let frameX = 0
+        let frameY = 0
+        let gameFrame = 0
+        const staggerFrames = 5
+        const spriteAnimations = []
+        const animationStates = [
+            { name: 'idle', frames: 7 },
+            { name: 'jump', frames: 7 }
+        ]
+        animationStates.forEach((state, i) => {
+            let frames = {
+                location: []
+            }
+            for (let j = 0; j<state.frames; j++) {
+                let positionX = j * spriteWidth
+                let positionY = i * spriteHeight
+                frames.location.push({x: positionX, y: positionY})
+            }
+            spriteAnimations[state.name] = frames
+        })
+        console.log(animationStates)
 
         function animate() {
             ctx.clearRect(0, 0, canvas_width, canvas_height)
-            ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+            let position = Math.floor(gameFrame / staggerFrames) % 6
+            frameX = spriteWidth * position
+            ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+
+            gameFrame++
             requestAnimationFrame(animate)
         }
         animate()
