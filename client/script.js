ef6591c96b4ff7951a6ad285390dfba931a0d9f5
Entita <entitak@gmai.com>
2021-07-28 19:32:25 +0200

    Change method of using animations so they're easily taken

--- client/script.js
+++ client/script.js
@@ -8,6 +8,18 @@ var app = new Vue({
     },
     created() {
         this.socket = io('https://gentle-island-28675.herokuapp.com/', { transports: ['websocket'] })
+        this.animationStates = [
+            { name: 'idle', frames: 7 },
+            { name: 'jump', frames: 7 },
+            { name: 'fall', frames: 7 },
+            { name: 'run', frames: 9 },
+            { name: 'dizzy', frames: 11 },
+            { name: 'sit', frames: 5 },
+            { name: 'roll', frames: 7 },
+            { name: 'bite', frames: 7 },
+            { name: 'ko', frames: 12 },
+            { name: 'hit', frames: 4 }
+        ]
     },
     mounted() {
         // this.context = document.getElementById('game').getContext('2d')
@@ -37,18 +49,6 @@ var app = new Vue({
         let gameFrame = 0
         const staggerFrames = 6
         const spriteAnimations = []
-        const animationStates = [
-            { name: 'idle', frames: 7 },
-            { name: 'jump', frames: 7 },
-            { name: 'fall', frames: 7 },
-            { name: 'run', frames: 9 },
-            { name: 'dizzy', frames: 11 },
-            { name: 'sit', frames: 5 },
-            { name: 'roll', frames: 7 },
-            { name: 'bite', frames: 7 },
-            { name: 'ko', frames: 12 },
-            { name: 'hit', frames: 4 }
-        ]
 
         animationStates.forEach((state, i) => {
             let frames = {
