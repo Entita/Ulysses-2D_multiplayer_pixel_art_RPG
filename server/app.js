a8f76e02c44f6e5d669d2b96d3767d10206c25b6
Entita <entitak@gmai.com>
2021-07-28 11:45:29 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    const path = __dirname.replace('\\server', '')
+    res.sendFile(path + '/client/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on :3000');
+});
