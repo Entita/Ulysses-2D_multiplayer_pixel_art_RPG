d6845f76193b29fc7548deda034b2a64fe1d930a
Entita <entitak@gmai.com>
2021-07-28 13:24:09 +0200

    Removing https

--- client/script.js
+++ client/script.js
@@ -1,3 +1,3 @@
-const socket = io('https://gentle-island-28675.herokuapp.com/', { "rejectUnauthorized" : false });
+const socket = io('http://gentle-island-28675.herokuapp.com/', { "rejectUnauthorized" : false });
 
 console.log('Socket: ', socket)
