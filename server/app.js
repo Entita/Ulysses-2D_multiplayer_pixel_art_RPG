e1120feeebe04a8b1dee8c857b40de5442564d21
Entita <entitak@gmai.com>
2021-07-29 17:24:01 +0200

    test

--- server/app.js
+++ server/app.js
@@ -6,15 +6,19 @@ var players = []
 
 io.on('connection', (socket) => {
     players.push(socket.id)
-    const new_data = {
+    const new_con_data = {
         'players': players,
         'socket_id': socket.id
     }
-    io.emit('user_connected', new_data)
-    
+    io.emit('user_connected', new_con_data)
+
     socket.on('disconnect', (socket) => {
         players = players.filter(item => item.socket_id !== socket.id)
-        io.emit('user_disconnected', new_data)
+        const new_dis_data = {
+            'players': players,
+            'socket_id': socket.id
+        }
+        io.emit('user_disconnected', new_dis_data)
     });
 
     socket.on('move', data => {
