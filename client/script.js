30c5dd5474a185966e879c78363a861751e7b527
Entita <entitak@gmai.com>
2021-07-29 09:15:45 +0200

    Changing animations and making the player look different directions

--- client/script.js
+++ client/script.js
@@ -102,10 +102,9 @@ var app = new Vue({
         /* Functions */
         function animateSprint() {
             ctx.clearRect(0, 0, canvas_width, canvas_height)
+            let position = 0
             if (player.moving) {
                 position = sprintX % spriteAnimations[player.sprite].location.length
-            } else {
-                position = 0
             }
             let frameX = player.sprite_width * position
             let frameY = spriteAnimations[player.sprite].location[position].y
