ad8ec15828b6cde71e422c53a2ea5d976625caf9
Entita <entitak@gmai.com>
2021-07-30 16:31:15 +0200

    test

--- client/script.js
+++ client/script.js
@@ -126,8 +126,8 @@ var app = new Vue({
                     image = new Image()
                 image.src = player.sprite_img
                 let position = player.moving ? sprintX % spriteAnimations[player.sprite].location.length : 0,
-                    frameX = player.sprite_width * position,
-                    frameY = spriteAnimations[player.sprite].location[position].y
+                    frameX = 0,
+                    frameY = 0
                 ctx.drawImage(image, frameX, frameY, player.sprite_width, player.sprite_height, player.x, player.y, player.width, player.height)
             }
             sprintX++
