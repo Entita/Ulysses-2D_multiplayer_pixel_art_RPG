1002b415da5938f347ea00c1c7df61c79ca84426
Entita <entitak@gmai.com>
2021-07-30 22:40:53 +0200

    human performance tests

--- server/app.js
+++ server/app.js
@@ -32,40 +32,6 @@ io.on('connection', socket => {
 
         socket.on('move', data => {
             var player = players[data.id]
-            // if (data.w) {
-            //     player.y = (player.y - player.speed) >= 0 ? player.y - player.speed : player.y
-            //     player.moving = true
-            //     player.sprite = 'up'
-            // }
-            // if (data.s) {
-            //     player.y = (player.y + player.speed) > (data.height - player.height) ? player.y : player.y + player.speed
-            //     player.moving = true
-            //     player.sprite = 'down'
-            // }
-            // if (data.a) {
-            //     player.x = (player.x - player.speed) >= 0 ? player.x - player.speed : player.x
-            //     player.moving = true
-            //     player.sprite = 'left'
-            // }
-            // if (data.d) {
-            //     player.x = (player.x + player.speed) > (data.width - player.width) ? player.x : player.x + player.speed
-            //     player.moving = true
-            //     player.sprite = 'right'
-            //     if (data.a && data.w) {
-            //         player.sprite = 'up'
-            //     } else if (data.a && data.s) {
-            //         player.sprite = 'down'
-            //     }
-            // }
-            // if (data.w && data.s && (!data.a && !data.d)) {
-            //     player.moving = false
-            // }
-            // if (data.a && data.d && (!data.w && !data.s)) {
-            //     player.moving = false
-            // }
-
-
-
             if (data.w) {
                 player.y = (player.y - player.speed) >= 0 ? player.y - player.speed : player.y
                 player.moving = true
@@ -85,7 +51,41 @@ io.on('connection', socket => {
                 player.x = (player.x + player.speed) > (data.width - player.width) ? player.x : player.x + player.speed
                 player.moving = true
                 player.sprite = 'right'
+                if (data.a && data.w) {
+                    player.sprite = 'up'
+                } else if (data.a && data.s) {
+                    player.sprite = 'down'
+                }
             }
+            if (data.w && data.s && (!data.a && !data.d)) {
+                player.moving = false
+            }
+            if (data.a && data.d && (!data.w && !data.s)) {
+                player.moving = false
+            }
+
+
+
+            // if (data.w) {
+            //     player.y = (player.y - player.speed) >= 0 ? player.y - player.speed : player.y
+            //     player.moving = true
+            //     player.sprite = 'up'
+            // }
+            // if (data.s) {
+            //     player.y = (player.y + player.speed) > (data.height - player.height) ? player.y : player.y + player.speed
+            //     player.moving = true
+            //     player.sprite = 'down'
+            // }
+            // if (data.a) {
+            //     player.x = (player.x - player.speed) >= 0 ? player.x - player.speed : player.x
+            //     player.moving = true
+            //     player.sprite = 'left'
+            // }
+            // if (data.d) {
+            //     player.x = (player.x + player.speed) > (data.width - player.width) ? player.x : player.x + player.speed
+            //     player.moving = true
+            //     player.sprite = 'right'
+            // }
             io.emit('update', players)
         })
 
