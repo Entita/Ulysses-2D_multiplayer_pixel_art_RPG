ed46e033e87f30be42be47af9c20790b3ae2995f
Entita <entitak@gmai.com>
2021-07-30 13:41:32 +0200

    test

--- client/script.js
+++ client/script.js
@@ -3,7 +3,12 @@ var app = new Vue({
     data() {
         return {
             socket: {},
-            animationStates: [],
+            animationStates: [
+                { name: 'down', frames: 4 },
+                { name: 'left', frames: 4 },
+                { name: 'right', frames: 4 },
+                { name: 'up', frames: 4 }
+            ],
             characters: [
                 'starlord',
                 'tonystark',
@@ -12,69 +17,64 @@ var app = new Vue({
                 'loki',
                 'deadpool',
                 'captainamerica'
-            ]
+            ],
+            isReady: false
         }
     },
     created() {
         this.socket = io('https://gentle-island-28675.herokuapp.com/', { transports: ['websocket'] })
-        this.animationStates = [
-            { name: 'down', frames: 4 },
-            { name: 'left', frames: 4 },
-            { name: 'right', frames: 4 },
-            { name: 'up', frames: 4 }
-        ]
     },
     mounted() {
         var this_ = this
-        var socketID
-        // Socket.io
-        this_.socket.on('position', data => {
-            console.log('position changed', data)
-        })
+        // var socketID
+        // // Socket.io
+        // this_.socket.on('position', data => {
+        //     console.log('position changed', data)
+        // })
 
-        this_.socket.on('players_updated', data => {
-            players = data
-        })
+        // this_.socket.on('players_updated', data => {
+        //     players = data
+        // })
 
-        this_.socket.on('user_connected', socket => {
-            socketID = this_.socket.id
-            const player = {
-                socket_id: socket.socket_id,
-                sprite: 'down',
-                spriteDir: 0,
-                x: 0,
-                y: 0,
-                sprite_img: 'img/sprite_starlord.png',
-                sprite_width: 32,
-                sprite_height: 48,
-                width: 67,
-                height: 100,
-                speed: 10,
-                moving: false
-            }
-            doPlayerUpdate(player)
+        // this_.socket.on('user_connected', socket => {
+        //     socketID = this_.socket.id
+        //     const player = {
+        //         socket_id: socket.socket_id,
+        //         sprite: 'down',
+        //         spriteDir: 0,
+        //         x: 0,
+        //         y: 0,
+        //         sprite_img: 'img/sprite_starlord.png',
+        //         sprite_width: 32,
+        //         sprite_height: 48,
+        //         width: 67,
+        //         height: 100,
+        //         speed: 10,
+        //         moving: false
+        //     }
+        //     doPlayerUpdate(player)
 
-            if (players.length === 0) {
-                // Load Sprites
-                this_.animationStates.forEach((state, i) => {
-                    let frames = {
-                        location: []
-                    }
-                    for (let j = 0; j < state.frames; j++) {
-                        let positionX = j * player.sprite_width
-                        let positionY = i * player.sprite_height
-                        frames.location.push({ x: positionX, y: positionY })
-                    }
-                    spriteAnimations[state.name] = frames
-                })
-            }
-            players.push(player)
-        })
+        //     if (players.length === 0) {
+        //         // Load Sprites
+        //         this_.animationStates.forEach((state, i) => {
+        //             let frames = {
+        //                 location: []
+        //             }
+        //             for (let j = 0; j < state.frames; j++) {
+        //                 let positionX = j * player.sprite_width
+        //                 let positionY = i * player.sprite_height
+        //                 frames.location.push({ x: positionX, y: positionY })
+        //             }
+        //             spriteAnimations[state.name] = frames
+        //         })
+        //     }
+        //     players.push(player)
+        // })
 
-        this_.socket.on('user_disconnected', data => {
-            console.log('player disconnected', data)
-            players = data
-        })
+        // this_.socket.on('user_disconnected', data => {
+        //     console.log('player disconnected', data)
+        //     players = data
+        // })
 
         // Canvas
         const canvas = document.getElementById('game'),
@@ -91,103 +91,105 @@ var app = new Vue({
         const spriteAnimations = [],
             keys = []
 
+        
+
         // Multiplayer
-        var players = []
+        // var players = []
 
-        startAnimating(fps)
+        // startAnimating(fps)
 
-        function startAnimating(fps) {
-            fpsInterval = 1000 / fps
-            then = Date.now()
-            startTime = then
-            animate()
-        }
+        // function startAnimating(fps) {
+        //     fpsInterval = 1000 / fps
+        //     then = Date.now()
+        //     startTime = then
+        //     animate()
+        // }
 
-        function animate() {
-            requestAnimationFrame(animate)
-            now = Date.now()
-            elapsed = now - then
-            if (elapsed > fpsInterval) {
-                then = now - (elapsed % fpsInterval)
+        // function animate() {
+        //     requestAnimationFrame(animate)
+        //     now = Date.now()
+        //     elapsed = now - then
+        //     if (elapsed > fpsInterval) {
+        //         then = now - (elapsed % fpsInterval)
 
-                animateSprint()
-            }
-        }
+        //         animateSprint()
+        //     }
+        // }
 
-        /* Functions */
-        function doPlayerUpdate(player) {
-            this_.socket.emit('update_player', player)
-        }
+        // /* Functions */
+        // // function doPlayerUpdate(player) {
+        // //     this_.socket.emit('update_player', player)
+        // // }
 
-        function animateSprint() {
-            ctx.clearRect(0, 0, canvas_width, canvas_height)
-            for (let index = 0; index < players.length; index++) {
-                let position = players[index].moving ? sprintX % spriteAnimations[players[index].sprite].location.length : 0
-                let frameX = players[index].sprite_width * position
-                let frameY = spriteAnimations[players[index].sprite].location[position].y
-                var image = new Image()
-                image.src = players[index].sprite_img
-                ctx.drawImage(image, frameX, frameY, players[index].sprite_width, players[index].sprite_height, players[index].x, players[index].y, players[index].width, players[index].height)
-            }
-            sprintX++
-            moveSprite()
-        }
+        // function animateSprint() {
+        //     ctx.clearRect(0, 0, canvas_width, canvas_height)
+        //     for (let index = 0; index < players.length; index++) {
+        //         let position = players[index].moving ? sprintX % spriteAnimations[players[index].sprite].location.length : 0
+        //         let frameX = players[index].sprite_width * position
+        //         let frameY = spriteAnimations[players[index].sprite].location[position].y
+        //         var image = new Image()
+        //         image.src = players[index].sprite_img
+        //         ctx.drawImage(image, frameX, frameY, players[index].sprite_width, players[index].sprite_height, players[index].x, players[index].y, players[index].width, players[index].height)
+        //     }
+        //     sprintX++
+        //     moveSprite()
+        // }
 
-        function movingSprint(player, dir) {
-            player.moving = true
-            player.sprite = dir
-            this_.socket.emit('move', players)
-        }
+        // // function movingSprint(player, dir) {
+        // //     player.moving = true
+        // //     player.sprite = dir
+        // //     this_.socket.emit('move', players)
+        // // }
 
-        function moveSprite() {
-            if (keys['w']) {
-                var thisPlayer = players[getCurrentPlayerIndex()]
-                thisPlayer.y = (thisPlayer.y - thisPlayer.speed) >= 0 ? thisPlayer.y - thisPlayer.speed : thisPlayer.y
-                movingSprint(thisPlayer, 'up')
-                doPlayerUpdate(thisPlayer)
-            }
-            if (keys['s']) {
-                var thisPlayer = players[getCurrentPlayerIndex()]
-                thisPlayer.y = (thisPlayer.y + thisPlayer.speed) > (canvas_height - thisPlayer.height) ? thisPlayer.y : thisPlayer.y + thisPlayer.speed
-                movingSprint(thisPlayer, 'down')
-                doPlayerUpdate(thisPlayer)
-            }
-            if (keys['a']) {
-                var thisPlayer = players[getCurrentPlayerIndex()]
-                thisPlayer.x = (thisPlayer.x - thisPlayer.speed) >= 0 ? thisPlayer.x - thisPlayer.speed : thisPlayer.x
-                movingSprint(thisPlayer, 'left')
-                doPlayerUpdate(thisPlayer)
-            }
-            if (keys['d']) {
-                var thisPlayer = players[getCurrentPlayerIndex()]
-                thisPlayer.x = (thisPlayer.x + thisPlayer.speed) > (canvas_width - thisPlayer.width) ? thisPlayer.x : thisPlayer.x + thisPlayer.speed
-                movingSprint(thisPlayer, 'right')
-                doPlayerUpdate(thisPlayer)
-            }
-        }
+        // function moveSprite() {
+        //     if (keys['w']) {
+        //         // var thisPlayer = players[getCurrentPlayerIndex()]
+        //         thisPlayer.y = (thisPlayer.y - thisPlayer.speed) >= 0 ? thisPlayer.y - thisPlayer.speed : thisPlayer.y
+        //         // movingSprint(thisPlayer, 'up')
+        //         // doPlayerUpdate(thisPlayer)
+        //     }
+        //     if (keys['s']) {
+        //         // var thisPlayer = players[getCurrentPlayerIndex()]
+        //         thisPlayer.y = (thisPlayer.y + thisPlayer.speed) > (canvas_height - thisPlayer.height) ? thisPlayer.y : thisPlayer.y + thisPlayer.speed
+        //         // movingSprint(thisPlayer, 'down')
+        //         // doPlayerUpdate(thisPlayer)
+        //     }
+        //     if (keys['a']) {
+        //         // var thisPlayer = players[getCurrentPlayerIndex()]
+        //         thisPlayer.x = (thisPlayer.x - thisPlayer.speed) >= 0 ? thisPlayer.x - thisPlayer.speed : thisPlayer.x
+        //         // movingSprint(thisPlayer, 'left')
+        //         // doPlayerUpdate(thisPlayer)
+        //     }
+        //     if (keys['d']) {
+        //         // var thisPlayer = players[getCurrentPlayerIndex()]
+        //         thisPlayer.x = (thisPlayer.x + thisPlayer.speed) > (canvas_width - thisPlayer.width) ? thisPlayer.x : thisPlayer.x + thisPlayer.speed
+        //         // movingSprint(thisPlayer, 'right')
+        //         // doPlayerUpdate(thisPlayer)
+        //     }
+        // }
 
-        function getCurrentPlayerIndex() {
-            return players.findIndex((obj => obj.socket_id == socketID))
-        }
+        // // function getCurrentPlayerIndex() {
+        // //     return players.findIndex((obj => obj.socket_id == socketID))
+        // // }
 
-        /* Event Listeners */
-        const dropdown = document.getElementById('animations')
-        dropdown.addEventListener('change', e => {
-            var thisPlayer = players[getCurrentPlayerIndex()]
-            thisPlayer.sprite_img = 'img/sprite_' + e.target.value + '.png'
-            doPlayerUpdate(thisPlayer)
-        })
+        // /* Event Listeners */
+        // const dropdown = document.getElementById('animations')
+        // dropdown.addEventListener('change', e => {
+        //     // var thisPlayer = players[getCurrentPlayerIndex()]
+        //     thisPlayer.sprite_img = 'img/sprite_' + e.target.value + '.png'
+        //     // doPlayerUpdate(thisPlayer)
+        // })
 
-        window.addEventListener('keydown', e => {
-            key = e.key.toLowerCase()
-            keys[key] = true
-        })
-        window.addEventListener('keyup', e => {
-            key = e.key.toLowerCase()
-            delete keys[key]
-            var thisPlayer = players[getCurrentPlayerIndex()]
-            thisPlayer.moving = false
-            doPlayerUpdate(thisPlayer)
-        })
+        // window.addEventListener('keydown', e => {
+        //     key = e.key.toLowerCase()
+        //     keys[key] = true
+        // })
+        // window.addEventListener('keyup', e => {
+        //     key = e.key.toLowerCase()
+        //     delete keys[key]
+        //     // var thisPlayer = players[getCurrentPlayerIndex()]
+        //     thisPlayer.moving = false
+        //     // doPlayerUpdate(thisPlayer)
+        // })
     }
 });
--- server/app.js
+++ server/app.js
@@ -2,43 +2,65 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server);
-var players = []
+var players = new Object()
 
 io.on('connection', socket => {
-    var player = {
-        socket_id: socket.id
-    }
-    players.push(player)
-    io.emit('user_connected', player)
-
-    socket.on('update_player', data => {
-        let index = players.findIndex((obj => obj.socket_id == data.socket_id))
-        players[index] = data
-        io.emit('players_updated', players)
-    })
-
-    socket.on('disconnect', () => {
-        players = players.filter(item => item.socket_id !== player.socket_id)
-        io.emit('user_disconnected', players)
-    });
-
-    socket.on('move', data => {
-        switch (data) {
-            case 'up':
-                io.emit('position', data)
-                break;
-            case 'down':
-                io.emit('position', data)
-                break;
-            case 'left':
-                io.emit('position', data)
-                break;
-            case 'right':
-                io.emit('position', data)
-                break;
+    socket.on('ready', () => {
+        const player = {
+            socket_id: socket.id,
+            sprite: 'down',
+            x: 0,
+            y: 0,
+            sprite_img: 'img/sprite_starlord.png',
+            sprite_width: 32,
+            sprite_height: 48,
+            width: 67,
+            height: 100,
+            speed: 10,
+            moving: false
         }
+        players[socket.id] = player
+        console.log(players)
     })
-});
+})
+
+// io.on('connection', socket => {
+//     var player = {
+//         socket_id: socket.id
+//     }
+//     players.push(player)
+//     io.emit('user_connected', player)
+
+//     socket.on('update_player', data => {
+//         let index = players.findIndex((obj => obj.socket_id == data.socket_id))
+//         players[index] = data
+//         io.emit('players_updated', players)
+//     })
+
+//     socket.on('disconnect', () => {
+//         players = players.filter(item => item.socket_id !== player.socket_id)
+//         io.emit('user_disconnected', players)
+//     });
+
+//     socket.on('move', data => {
+//         switch (data) {
+//             case 'up':
+//                 io.emit('position', data)
+//                 break;
+//             case 'down':
+//                 io.emit('position', data)
+//                 break;
+//             case 'left':
+//                 io.emit('position', data)
+//                 break;
+//             case 'right':
+//                 io.emit('position', data)
+//                 break;
+//         }
+//     })
+// });
+
+
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening ...');
