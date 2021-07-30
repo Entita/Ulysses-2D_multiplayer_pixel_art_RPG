38bcaf781b7643e000b854f8533b398077365241
Entita <entitak@gmai.com>
2021-07-31 00:15:26 +0200

    Change particles

--- client/script.js
+++ client/script.js
@@ -141,7 +141,8 @@ var app = new Vue({
             };
 
             // Size our particle
-            this.radius = 5 + Math.random() * 5;
+            this.size = 4;
+            this.opacity = 1
 
             // Set a max time to live for our particle
             this.life = 30 + Math.random() * 10;
@@ -152,16 +153,17 @@ var app = new Vue({
                 let p = this;
 
                 if (this.remainingLife > 0
-                    && this.radius > 0) {
+                    && this.size > 0) {
                     // Draw a circle at the current location
                     ctx.beginPath();
-                    ctx.arc(p.startX, p.startY, p.radius, 0, Math.PI * 2);
-                    ctx.fillStyle = "rgba(" + this.rgbArray[0] + ',' + this.rgbArray[1] + ',' + this.rgbArray[2] + ", 1)";
+                    ctx.arc(p.startX, p.startY, p.size, p.size);
+                    ctx.fillStyle = "rgba(" + this.rgbArray[0] + ',' + this.rgbArray[1] + ',' + this.rgbArray[2] + ", " + this.opacity + ")";
                     ctx.fill();
 
                     // Update the particle's location and life
                     p.remainingLife--;
-                    p.radius -= 0.25;
+                    p.size -= 0.2;
+                    this.opacity -= 0.05;
                     p.startX += p.speed.x;
                     p.startY += p.speed.y;
                 }
