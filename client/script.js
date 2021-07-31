cf2ffdf55a3858ed7e846d782cc77079d9e334ed
Entita <entitak@gmai.com>
2021-07-31 12:57:09 +0200

    test

--- client/script.js
+++ client/script.js
@@ -179,7 +179,7 @@ var app = new Vue({
 
                 var lines = wrapText(message, messages_width - messages_font);
                 lines.forEach(function (line, i) {
-                    messages_ctx.fillText(line, (thisPlayer.x - messages_width) / 2, ((i + 1) * messages_font) + thisPlayer.y);
+                    messages_ctx.fillText(line, thisPlayer.x + ((thisPlayer.x - messages_width) / 2), ((i + 1) * messages_font) + thisPlayer.y);
                 });
             }
         }
