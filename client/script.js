dd68715a91a170599948c9e0e99a0a34954edbe6
Entita <entitak@gmai.com>
2021-08-03 18:23:58 +0200

    test

--- client/script.js
+++ client/script.js
@@ -460,41 +460,27 @@ var app = new Vue({
         }
 
         function validMove(x, y, keys) {
-            if (keys['w']) {
-                for (let i = 0; i < players[socketID].width; i++) {
-                    var color = collision_ctx.getImageData(x + i, y - 1, 1, 1)
-                    if (color.data[0] != 0 && color.data[1] != 0 && color.data[2] != 0) {
-                        return false
-                    }
-                }
-                return true
-            } else if (keys['s']) {
-                for (let i = 0; i < players[socketID].width; i++) {
-                    var color = collision_ctx.getImageData(x + i, y + 1, 1, 1)
-                    if (color.data[0] != 0 && color.data[1] != 0 && color.data[2] != 0) {
-                        return false
-                    }
-                }
-                return true
-            } else if (keys['a']) {
-                for (let i = 0; i < players[socketID].width; i++) {
-                    var color = collision_ctx.getImageData(x + i - 1, y, 1, 1)
-                    if (color.data[0] != 0 && color.data[1] != 0 && color.data[2] != 0) {
-                        return false
-                    }
-                }
-                return true
-            } else if (keys['d']) {
-                for (let i = 0; i < players[socketID].width; i++) {
-                    var color = collision_ctx.getImageData(x + i + 1, y, 1, 1)
-                    if (color.data[0] != 0 && color.data[1] != 0 && color.data[2] != 0) {
-                        return false
-                    }
-                }
-                return true
-            } else {
-                return false
-            }
+            // if (keys['w']) {
+            //     for (let i = 0; i < players[socketID].width; i++) {
+            //         var color = collision_ctx.getImageData(x + i, y - 1, 1, 1)
+            //         if (color.data[0] != 0 && color.data[1] != 0 && color.data[2] != 0) {
+            //             return false
+            //         }
+            //     }
+            //     return true
+            // } else if (keys['s']) {
+            //     var color = collision_ctx.getImageData(x, y + 1, 1, 1)
+            //     return true
+            // } else if (keys['a']) {
+            //     var color = collision_ctx.getImageData(x - 1, y, 1, 1)
+            //     return true
+            // } else if (keys['d']) {
+            //     var color = collision_ctx.getImageData(x + 1, y - 1, 1, 1)
+            //     return true
+            // } else {
+            //     return false
+            // }
+            return true
         }
 
         function createParticleAtPoint(x, y, colorData) {
