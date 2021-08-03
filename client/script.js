6f5197ccbb431f103c8bd93c137dea87cae66239
Entita <entitak@gmai.com>
2021-08-03 15:32:01 +0200

    test

--- client/script.js
+++ client/script.js
@@ -217,13 +217,13 @@ var app = new Vue({
             pseudoCanvas.height = canvas_height
             pseudoCtx = pseudoCanvas.getContext('2d')
             for (let x = 0; x < width; x++) {
-                //     for (let y = 0; x < height; y++) {
-                if (map[x][0] === 1) {
-                    pseudoCtx.drawImage(world_sheet, 32, 0, 32, 32, x * 32, 0, 32, 32)
-                } else {
-                    pseudoCtx.drawImage(world_sheet, 0, 0, 32, 32, x * 32, 0, 32, 32)
+                for (let y = 0; x < height; y++) {
+                    if (map[x][0] === 1) {
+                        pseudoCtx.drawImage(world_sheet, 32, 0, 32, 32, x * 32, 0, 32, 32)
+                    } else {
+                        pseudoCtx.drawImage(world_sheet, 0, 0, 32, 32, x * 32, 0, 32, 32)
+                    }
                 }
-                //     }
             }
 
             world_ctx.clearRect(0, 0, canvas_width, canvas_height)
