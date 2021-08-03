972bc32d8ace53ca2b74fd644592aeb21f57549f
Entita <entitak@gmai.com>
2021-08-03 14:09:24 +0200

    test

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ function createWorld(width, height, pixel) {
     for (let x = 0; x < width; x++) {
         map[x] = []
         for (let y = 0; y < height; y++) {
-            if (Math.random > .75) {
+            if (Math.random() > .75) {
                 map[x][y] = 1
             } else {
                 map[x][y] = 0
