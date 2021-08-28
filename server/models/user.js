3921978909847eb0e1e6185858c3c4ec170c805c
Entita <entitak@gmai.com>
2021-08-29 00:37:38 +0200

    test

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const moment = require('moment')
 const Schema = mongoose.Schema
 
-const createdTime = moment(Date.now()).format('h:mm a, MMMM Do YYYY')
+const createdTime = moment(new Date()).format('h:mm a, MMMM Do YYYY')
 
 const userSchema = new Schema({
     nickname: {
