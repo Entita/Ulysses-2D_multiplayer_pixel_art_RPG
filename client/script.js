a2f5fe154ee5ca474254eab413c116e490117422
Entita <entitak@gmai.com>
2021-08-28 23:42:22 +0200

    test

--- client/script.js
+++ client/script.js
@@ -32,6 +32,7 @@ var app = new Vue({
     created() {
         this.socket = io('https://gentle-island-28675.herokuapp.com/', { transports: ['websocket'] })
         this.loginSocket = JSON.parse(localStorage.getItem('loginToken'))
+        console.log(this.loginSocket)
     },
     mounted() {
         var this_ = this,
