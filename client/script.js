fa68659b61b8ba925d23d4e228f6ca392b79c447
Entita <entitak@gmai.com>
2021-08-29 23:07:32 +0200

    test

--- client/script.js
+++ client/script.js
@@ -715,6 +715,7 @@ var app = new Vue({
                     exp: 0,
                     account_id: this.loginSocket.id
                 }
+                console.log(character, this.loginSocket.id)
                 this.socket.emit('addCharacter', character)
                 this.startingMenu.creatingCharacter = false
             } else {
