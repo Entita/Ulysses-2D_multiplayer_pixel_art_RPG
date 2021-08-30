d4b4f58f92e0cf79f01d043aa954793d5aeb1436
Entita <entitak@gmai.com>
2021-08-30 18:45:15 +0200

    test

--- client/script.js
+++ client/script.js
@@ -730,7 +730,7 @@ var app = new Vue({
             const name = document.getElementById('character_name').value
             if (name.length > 4) {
                 const character = {
-                    id: this.loginSocket.characters.length + 1,
+                    id: this.loginSocket.characters.length,
                     name: name,
                     skin: skin,
                     exp: 0,
