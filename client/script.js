ff06e69ae9a4ae3c8f7ed95eb03cd2e3d975cd4d
Entita <entitak@gmai.com>
2021-09-01 00:33:33 +0200

    test

--- client/script.js
+++ client/script.js
@@ -51,11 +51,12 @@ var app = new Vue({
 
         this.socket.on('logOutUser', accont_id => {
             if (this.loginSocket && this.loginSocket.id === accont_id) {
-                alert('Logged out, someone logged in')
+                // alert('Logged out, someone logged in')
                 localStorage.removeItem('loginSocket')
                 this.loginSocket = null
                 this.startingMenu.creatingCharacter = false
                 this.isReady = false
+                this.socket.emit('disconnect')
             }
         })
     },
