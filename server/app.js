cdbe347677d408b1e7b5431fc17c9b3a226e502d
Entita <entitak@gmai.com>
2021-08-31 23:37:15 +0200

    test

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const server = require('http').createServer(app)
 const io = require("socket.io")(server)
 const moment = require('moment')
 const nodeMailer = require('nodemailer')
-const path = require('path')
 
 // MongoDN
 const mongoose = require('mongoose')
