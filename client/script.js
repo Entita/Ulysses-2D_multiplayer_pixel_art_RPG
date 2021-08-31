3b64ba14e80682bcd6c070fe20cb7c897631212c
Entita <entitak@gmai.com>
2021-09-01 01:23:43 +0200

    test

--- client/script.js
+++ client/script.js
@@ -45,6 +45,7 @@ var app = new Vue({
         }
 
         this.socket.on('update_user', data => {
+            console.log(this.loginSocket, data, this.loginSocket.id === data.account_id, data.type === 'update')
             if (this.loginSocket && this.loginSocket.id === data.account_id) {
                 // Update all people on same account
                 if (data.type === 'logout') {
